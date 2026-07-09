aa0937bac15b4b5197e9e23b0fae1dbbc1434cba
wtsolutions <he.yang@wtsolutions.cn>
2026-07-09 18:51:14 +0800

    update template to shibuya

--- source/conf.py
+++ source/conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Sheet-to-Doc'
+project = 'Sheet-to-Doc Documentation'
 copyright = '2022~2026, WTSolutions'
 author = 'WTSolutions'
 release = '2.14.0.0'
@@ -63,5 +63,30 @@ source_suffix = {
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 # html_theme = 'alabaster'
-html_theme = 'sphinx_rtd_theme'
+# html_theme = 'sphinx_rtd_theme'
+# html_static_path = ['_static']
+
+html_theme = "shibuya"
 html_static_path = ['_static']
+
+# 自定义导航链接
+# shibuya 主题的导航链接配置项
+html_theme_options = {
+    "nav_links": [
+        {
+            "title": "Product File",
+            "url": "https://s.wtsolutions.cn/sheet-to-doc-product.html"
+        },
+        {
+            "title": "Sheet-to-Doc Web Version",
+            "url": "https://s.wtsolutions.cn/sheet-to-doc.html"
+        },
+    ]
+}
+
+html_context = {
+    "languages": [
+        ("English", "/en/%s/", "en"),
+        ("中文", "/zh-cn/%s/", "zh-cn"),
+    ]
+}
